73a12d5ce2ae7e8369a1ae706e2961fb18c9a778
osamaayman1005 <50743464+osamaayman1005@users.noreply.github.com>
2021-01-17 18:43:06 +0200

    Update team.dart
    adding loading screen
    adding wating for players
    adding error message
    updating some style

--- lib/screens/team/team.dart
+++ lib/screens/team/team.dart
@@ -1,9 +1,12 @@
 //TODO implement team screen(s)
 import 'package:el_joker/models/user.dart';
+import 'package:el_joker/screens/team/waiting.dart';
 import 'package:el_joker/services/auth.dart';
 import 'package:el_joker/services/database.dart';
 import 'package:el_joker/shared/constants.dart';
+import 'package:el_joker/shared/loading.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:provider/provider.dart';
 
 class Team extends StatefulWidget {
@@ -19,12 +22,25 @@ class _TeamState extends State<Team> {
   String activity = '';
   int totalPlayers = 0;
   int currentPlayers = 0;
+  double longitude=0;
+  double latitude=0;
+  bool loading = false;
 
+void getCurrentLocation() async{
+  LocationPermission permission = await Geolocator.requestPermission();
+  Position location = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  print(location);
+  setState(() {
+    longitude=location.longitude;
+    latitude=location.latitude;
+  });
+}
   @override
   Widget build(BuildContext context) {
     User user = Provider.of<User>(context);
     DatabaseService _database = DatabaseService(uid: user.uid);
-    return Scaffold(
+    //toggle between waiting for players and game settings screen
+    return loading?Loading():Scaffold(
       appBar: AppBar(
         title: Text('Game '),
         backgroundColor: Colors.indigo,
@@ -34,6 +50,7 @@ class _TeamState extends State<Team> {
           key: _formKey,
           child: Column(
             children: [
+              SizedBox(height: 30,),
               TextFormField(
                   decoration: TextInputDecoration.copyWith(
                       hintText: 'activity'),
@@ -48,13 +65,15 @@ class _TeamState extends State<Team> {
                 height: 20,
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   SizedBox(
                     width: 150,
                     child: TextFormField(
                       decoration: TextInputDecoration.copyWith(
                           hintText: 'current players'),
-
+                      validator: (val) =>
+                      int.tryParse(val) == null ||val.isEmpty? 'enter a number' : null,
                       onChanged: (val) {
                         setState(() {
                           currentPlayers = int.tryParse(val);
@@ -62,13 +81,18 @@ class _TeamState extends State<Team> {
                       },
                     ),
                   ),
-                  Text('/'),
+                  Text('/',
+                    style: TextStyle(
+                      fontSize: 20.0
+                    ),
+                  ),
                   SizedBox(
                     width: 150,
                     child: TextFormField(
                       decoration:
                       TextInputDecoration.copyWith(hintText: 'total players'),
-
+                      validator: (val) =>
+                      int.tryParse(val) == null ||val.isEmpty? 'enter a number' : null,
                       onChanged: (val) {
                         setState(() {
                           totalPlayers = int.tryParse(val);
@@ -85,10 +109,20 @@ class _TeamState extends State<Team> {
                 height: 60,
                 width: 320,
                 child: RaisedButton(
+                  elevation: 5.0,
                   onPressed: () async {
+                    if(currentPlayers>=totalPlayers){
+                      setState(() {
+                        error='please enter correct players numbers';
+                      });
+                      return;
+                    }
                     if (_formKey.currentState.validate()) {
-
-                  dynamic result = await _database.updateGameData(activity, totalPlayers, currentPlayers);
+                      setState(() {
+                        loading = true;
+                      });
+                  await getCurrentLocation();
+                  dynamic result = await _database.updateGameData(activity, totalPlayers, currentPlayers,longitude,latitude);
 
                       if (result == null) {
                         setState(() {
@@ -96,6 +130,10 @@ class _TeamState extends State<Team> {
                           error = 'please enter a valid activity';
                         });
                       }
+                      setState(() {
+                        loading = false;
+                      });
+                      Navigator.pushNamed(context, '/waitingForPlayers');
                     }
                   },
                   color: Colors.indigo,
@@ -104,7 +142,12 @@ class _TeamState extends State<Team> {
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
+
               ),
+              Text(error,
+              style: TextStyle(
+                color: Colors.red,
+              ),),
             ],
           ),
         ),
