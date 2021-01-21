5b9ae6fc1cd64a0879ca80a074b8113c3ea253f5
osamaayman1005 <50743464+osamaayman1005@users.noreply.github.com>
2021-01-21 02:24:29 +0200

    calculate distance between the user and the team

--- lib/screens/player/game_card.dart
+++ lib/screens/player/game_card.dart
@@ -1,18 +1,27 @@
 //todo ask user about number of jokers/players (if a user is bringing someone else with them) &make sure that the user can only click one
 
 import 'package:el_joker/models/game.dart';
+import 'package:el_joker/models/user.dart';
 import 'package:el_joker/services/database.dart';
+import 'package:el_joker/shared/loading.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:provider/provider.dart';
 
 class GameCard extends StatelessWidget {
-
+  final double distance;
   final Game game;
 
-  GameCard({this.game});
+  GameCard({this.game,this.distance});
+
+
 
   @override
   Widget build(BuildContext context) {
     DatabaseService _database = DatabaseService(uid:game.creatorId);
+
+
+
     return Padding(
       padding: EdgeInsets.only(top: 8.0),
       child: Card(
@@ -23,12 +32,13 @@ class GameCard extends StatelessWidget {
             backgroundColor: Colors.blue,
           ),
           title: Text(game.activity),
-          subtitle: Text('${game.currentPlayers}/${game.totalPlayers}'),
+          subtitle: Text('${game.currentPlayers}/${game.totalPlayers} distance: ${distance}km'),
         onTap: (){
             if(game.currentPlayers<game.totalPlayers){
             _database.updateGameData(game.activity, game.totalPlayers, game.currentPlayers+1, game.locationX, game.locationY);
             print(game.creatorId);
-            Navigator.pushNamed(context, '/gameLocation');
+           Navigator.pushNamed(context, '/gameLocation');
+
             //todo handle max/max number of players
         }
             else{
--- lib/screens/player/game_list.dart
+++ lib/screens/player/game_list.dart
@@ -1,7 +1,12 @@
 //TODO implement player screen(s)
+import 'dart:io';
+
 import 'package:el_joker/models/game.dart';
+import 'package:el_joker/models/user.dart';
 import 'package:el_joker/screens/player/game_card.dart';
+import 'package:el_joker/shared/loading.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:provider/provider.dart';
 import 'package:el_joker/services/database.dart';
 
@@ -11,13 +16,27 @@ class GameList extends StatefulWidget {
 }
 
 class _GameListState extends State<GameList> {
+  double locationX=0;
+  double locationY=0;
+  Future distanceInMeters()async{
+    LocationPermission permission = await Geolocator.requestPermission() ;
+    Position location = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high) ;
+    locationX = await location.longitude;
+     locationY = await location.latitude;
+    print(locationY);
+    print(locationX);
+    }
   @override
   Widget build(BuildContext context) {
+    distanceInMeters();
+    //todo fix the first  infinite load each run
     final games = Provider.of<List<Game>>(context) ??[];
-    return ListView.builder(
+    return locationX==0?Loading():ListView.builder(
         itemCount: games.length,
         itemBuilder:(context,index){
-          return GameCard(game: games[index]);
+        double distance = Geolocator.distanceBetween(locationY, locationX, games[index].locationY, games[index].locationX);
+        print(distance);
+          return GameCard(game: games[index],distance: distance/1000,);
         }
     );
   }
